5bd4079fb55ff045172eb65c806db402eded7394
Denlich <muchalovych@gmail.com>
2023-11-28 19:41:03 +0100

    Add prisma

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.listen(PORT, () => console.log(`App is listening on port: ${PORT}`));
